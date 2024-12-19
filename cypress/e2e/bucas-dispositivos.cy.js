893b955a1d6b4d1297baa5a7275c8f7ef04442e8
Bruno Guerreiro <brunoguerreiro.campos@hotmail.com>
2024-12-19 17:43:29 -0300

    api/cypress

---
+++ cypress/e2e/bucas-dispositivos.cy.js
@@ -0,0 +1,32 @@
+///<reference types="cypress"/>
+
+describe('Buscar Dispositivos', () => {
+
+    it('Buscar dispositivo especifico', () => {
+        cy.request({
+            method: "GET",
+            url: "https://api.restful-api.dev/objects/3"
+        })
+            .then((resultado) => {
+                //Será executado quando a requisição responder
+                expect(resultado.status).equal(200)
+                expect(resultado.body.id).equal('3')
+                expect(resultado.body.id).not.empty
+                expect(resultado.body.name).equal('Apple iPhone 12 Pro Max')
+
+            })
+    })
+
+    it('Buscar dispositivo inexistente', () => {
+        cy.request({
+            method: "GET",
+            url: "https://api.restful-api.dev/objects/iddobruno",
+            failOnStatusCode: false
+        })
+            .then((resultado) => {
+                //Será executado quando a requisição responder
+                expect(resultado.status).equal(404)
+                expect(resultado.body.error).equal('Oject with id=iddobruno was not found.')
+            })
+    })
+})
